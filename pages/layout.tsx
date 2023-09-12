3f8253ea1eb404051634f6772f7a2a84b8c8841e
Alan <alan_s_pereira@hotmail.com>
2023-09-12 11:29:04 -0300

    Preparações de deploy

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,12 +1,7 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { metadata } from "@/src/services/metadata";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Nerdolar",
-  description: "A casa dos nerdolas",
-};
 
 export default function RootLayout({
   children,
@@ -15,6 +10,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <metadata />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
---
+++ src/services/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Nerdolar",
+  description: "A casa dos nerdolas",
+};
